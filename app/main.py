922eb7591c00ee060916e27e513c115294fd07f4
Kieran-11 <157422842+Kieran-11@users.noreply.github.com>
2024-12-04 15:31:31 -0500

    Debugging main.py

--- app/main.py
+++ app/main.py
@@ -5,23 +5,26 @@ from typing import Optional
 from pydantic import BaseModel
 import json
 import os
+import mysql
 import mysql.connector
 from mysql.connector import Error
-
-app = FastAPI()
 from fastapi.middleware.cors import CORSMiddleware
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+
 
 DBHOST = "ds2022.cqee4iwdcaph.us-east-1.rds.amazonaws.com"
 DBUSER = "admin"
 DBPASS = os.getenv('DBPASS')
 DB = "rrx5eg"
 
+app = FastAPI()
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 @app.get("/")  # zone apex
 def zone_apex():
     return {"Good Day": "Sunshine!"}
@@ -36,9 +39,9 @@ def multiply(c: int, d: int):
 
 @app.get('/genres')
 def get_genres():
+    query = "SELECT * FROM genres ORDER BY genreid;"
     db = mysql.connector.connect(user=DBUSER, host=DBHOST, password=DBPASS, database=DB)
     cur=db.cursor()
-    query = "SELECT * FROM genres ORDER BY genreid;"
     try:    
         cur.execute(query)
         headers=[x[0] for x in cur.description]
@@ -46,28 +49,32 @@ def get_genres():
         json_data=[]
         for result in results:
             json_data.append(dict(zip(headers,result)))
-        return(json_data)
-    except Error as e:
-        return {"Error": "MySQL Error: " + str(e)}
-    finally:
         cur.close()
         db.close()
+        return(json_data)
+    except Error as e:
+        print("MySQL Error: ", str(e))
+        cur.close()
+        db.close()
+        return {"Error": "MySQL Error: " + str(e)}
     
 @app.get('/songs')
-def get_genres():
+def get_songs():
+    query = "Select * From songs ORDER BY id;"
     db = mysql.connector.connect(user=DBUSER, host=DBHOST, password=DBPASS, database=DB)
     cur=db.cursor()
-    query = "SELECT * FROM songs ORDER BY id;"
-    try:    
+    try:
         cur.execute(query)
         headers=[x[0] for x in cur.description]
         results = cur.fetchall()
         json_data=[]
         for result in results:
             json_data.append(dict(zip(headers,result)))
-        return(json_data)
-    except Error as e:
-        return {"Error": "MySQL Error: " + str(e)}
-    finally: 
         cur.close()
         db.close()
+        return(json_data)
+    except Error as e:
+        print("MySQL Error: ", str(e))
+        cur.close()
+        db.close()
+        return {"Error": "MySQL Error: " + str(e)}
